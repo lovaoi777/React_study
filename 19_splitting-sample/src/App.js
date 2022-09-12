7cc6100dff94e30b70550085884856c8a1944e73
lovaoi777 <lovaoi777@naver.com>
2022-09-12 14:36:19 +0900

    splitting

--- 19_splitting-sample/src/App.js
+++ 19_splitting-sample/src/App.js
@@ -1,25 +1,80 @@
+import React, { useState, Suspense } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import loadable from '@loadable/component'
+// const SplitMe = React.lazy(() => import('./SplitMe'));
+const SplitMe = loadable(() => import('./SplitMe'),{
+  fallback : <div>loading...</div>
+});
+
+// import notify from './notify';
+
+// function App() {
+//   //저바스크립트 함수 비동기 로딩
+//   // const onClick = () => {
+//   //   import('./notify').then(result=>result.default());
+//   // }import SplitMe from '../SplitMe';
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         {/* <p onClick={onClick}>Hello React!</p> */}
+//       </header>
+//     </div>
+//   );
+// }
+
+
+
+// State를 사용한  컴포넌트 스플리팅
+// class App extends Component{
+//   state = { 
+//     SplitMe : null
+//   };
+//   handleClick = async () => { 
+//     const loadaModule = await import ('./SplitMe');
+//     this.setState({
+//       SplitMe : loadaModule.default
+//     });
+//   };
+//   render(){
+//     const {SplitMe} = this.state;
+//     return (
+//       <div className='App'>
+//         <header className='App-header'>
+//           <img src={logo} className='App-logo' />
+//           <p onClick={this.handleClick}>Hello React!</p>
+//           {SplitMe && <SplitMe />}
+//         </header>
+//       </div>
+//     )
+//   }
+// }
+
+
+//React.lazy와 Suspense 사용하기
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  const [visible, setVisible] = useState(false);
+  const onClick = () => { 
+    setVisible(true);
+  }
+  const onMouseOver = () => { 
+    SplitMe.preload();
+  }
+  return(
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p onClick={onClick} onMouseOver={onMouseOver}>Hello React!</p>
+        {/* <Suspense fallback={<div>loading....</div>}> */}
+          {visible && <SplitMe />}
+        {/* </Suspense> */}
       </header>
     </div>
-  );
-}
+  )
+};
 
 export default App;
