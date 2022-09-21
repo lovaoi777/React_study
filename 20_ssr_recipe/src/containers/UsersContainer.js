d7f5f27e2ceac19320fb4f829a68e713cb998938
lovaoi777 <lovaoi777@naver.com>
2022-09-21 14:02:07 +0900

    ssr

--- 20_ssr_recipe/src/containers/UsersContainer.js
+++ 20_ssr_recipe/src/containers/UsersContainer.js
@@ -1,14 +1,20 @@
 import React , {useEffect} from 'react';
 import Users from '../components/Users';
 import { getUsers } from '../modules/users';
-import {connect} from 'react-redux'
+import {connect} from 'react-redux';
+import {Preloader} from '../lib/PreloadContext'
 const UsersContainer = ({users, getUsers}) => {
     // 컴포넌트가 마운트되고 나서 호출
     useEffect(()=>{
         if(users) return; //users가 이미 유효하다면 요청하지 않음
         getUsers();
     },[getUsers, users]);
-    return <Users users={users} />
+    return( 
+    <>
+    <Users users={users} />
+    <Preloader resolve={getUsers} />
+    </>
+    )
 };
 
 export default connect(
